e68444be6d55b529dfd302c7da80f278456a2b21
kyle <kyle@archimedes.galaxy>
2026-03-29 19:38:38 -0700

    i hate windows

--- build.bat
+++ build.bat
@@ -9,18 +9,18 @@ del /f /q bdsm.spec
 
 powershell -Command "(Get-Content bdsm.py) -replace 'RUN_AS_EXE = False', 'RUN_AS_EXE = True' | Set-Content bdsm.py"
 
-pyinstaller --hidden-import=yaml ^
-            --hidden-import=patool ^
-            --hidden-import=patoolib ^
-            --hidden-import=patoolib.programs ^
-            --hidden-import=patoolib.programs.p7zip ^
-            --hidden-import=patoolib.programs.unzip ^
-            --hidden-import=patoolib.programs.tar ^
-            --hidden-import=patoolib.programs.gzip ^
-            --hidden-import=patoolib.programs.bzip2 ^
-            --hidden-import=patoolib.programs.zip ^
-            --hidden-import=patoolib.programs.unar ^
-            --onefile --windowed --name bdsm gui.py
+python -m PyInstaller --hidden-import=yaml ^
+                      --hidden-import=patool ^
+                      --hidden-import=patoolib ^
+                      --hidden-import=patoolib.programs ^
+                      --hidden-import=patoolib.programs.p7zip ^
+                      --hidden-import=patoolib.programs.unzip ^
+                      --hidden-import=patoolib.programs.tar ^
+                      --hidden-import=patoolib.programs.gzip ^
+                      --hidden-import=patoolib.programs.bzip2 ^
+                      --hidden-import=patoolib.programs.zip ^
+                      --hidden-import=patoolib.programs.unar ^
+                      --onefile --windowed --name bdsm gui.py
 
 mkdir dist\utils
 xcopy /e /i resources dist\utils\resources
